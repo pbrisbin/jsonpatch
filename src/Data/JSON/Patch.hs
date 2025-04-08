fd95db13400338a94150e11a44470fd6250e2472
patrick brisbin <pbrisbin@gmail.com>
2025-04-08 14:37:07 -0400

    Add license headers via headroom

--- src/Data/JSON/Patch.hs
+++ src/Data/JSON/Patch.hs
@@ -1,3 +1,11 @@
+-- |
+--
+-- Module      : Data.JSON.Patch
+-- Copyright   : (c) 2025 Patrick Brisbin
+-- License     : AGPL-3
+-- Maintainer  : pbrisbin@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
 module Data.JSON.Patch
   ( Patch
   , applyPatches
--- test/Data/JSON/PatchSpec.hs
+++ test/Data/JSON/PatchSpec.hs
@@ -1,3 +1,11 @@
+-- |
+--
+-- Module      : Data.JSON.PatchSpec
+-- Copyright   : (c) 2025 Patrick Brisbin
+-- License     : AGPL-3
+-- Maintainer  : pbrisbin@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
 module Data.JSON.PatchSpec
   ( spec
   ) where
