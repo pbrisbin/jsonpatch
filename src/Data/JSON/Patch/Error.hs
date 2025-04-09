847ea1d215316671695d4d797fa12b60e7db2353
patrick brisbin <pbrisbin@gmail.com>
2025-04-09 16:59:08 -0400

    Use data in error testing

--- src/Data/JSON/Patch/Error.hs
+++ src/Data/JSON/Patch/Error.hs
@@ -9,13 +9,41 @@ import Data.Aeson (Value)
 import Data.JSON.Pointer
 import Data.JSON.Pointer.Token
 import Data.Vector (Vector)
+import Data.Vector qualified as V
 
 data PatchError
   = ParseError Value String
-  | TestFailed Pointer Value Value
   | PointerNotFound [Token] (Maybe String)
   | InvalidObjectOperation [Token] Value
   | IndexOutOfBounds [Token] Int (Vector Value)
   | EmptyArray [Token]
+  | TestFailed Pointer Value Value
   deriving stock (Show)
-  deriving anyclass (Exception)
+
+instance Exception PatchError where
+  displayException = \case
+    ParseError v msg ->
+      "Unable to parse Patch(es) from Value: "
+        <> ("\n  error: " <> msg)
+        <> ("\n  input: " <> show v)
+    PointerNotFound ts mType ->
+      "Path "
+        <> tokensToString ts
+        <> " does not exist"
+        <> maybe "" (" or is not " <>) mType
+    InvalidObjectOperation ts _v ->
+      "Cannot perform object operation on non-object at " <> tokensToString ts
+    IndexOutOfBounds ts n vec ->
+      "Index "
+        <> show n
+        <> " is out of bounds for vector of length "
+        <> show (V.length vec)
+        <> " at "
+        <> tokensToString ts
+    EmptyArray ts ->
+      "Cannot perform operation on empty array at " <> tokensToString ts
+    TestFailed p actual expected ->
+      "Test failed at "
+        <> pointerToString p
+        <> ("\n  expected: " <> show expected)
+        <> ("\n    actual: " <> show actual)
