1bfbc525944286ca3edb10de6b762e3180de9c75
patrick brisbin <pbrisbin@gmail.com>
2025-04-09 11:07:18 -0400

    More

--- src/Data/Aeson/Optics/Ext.hs
+++ src/Data/Aeson/Optics/Ext.hs
@@ -10,18 +10,39 @@ import Data.Aeson (Key, Value (..))
 import Data.Aeson.Key qualified as Key
 import Data.Aeson.KeyMap qualified as KeyMap
 import Data.Aeson.Optics
+import Data.Vector (Vector)
 import Data.Vector qualified as V
 import Data.Vector.Ext qualified as V
 import Optics
 
+-- | Like 'key', but uses 'at' instead of 'ix'. This is handy when adding and
+-- removing object keys:
+--
+-- >>> "{\"a\": 100, \"b\": 200}" & atKey "a" .~ Nothing
+-- "{\"b\":200}"
+--
+-- >>> "{\"a\": 100, \"b\": 200}" & atKey "c" ?~ String "300"
+-- "{\"a\":100,\"b\":200,\"c\":\"300\"}"
 atKey :: Key -> AffineTraversal' Value (Maybe Value)
 atKey k = _Object % at k
 
+-- | Like 'atKey' but for 'Array'
+--
+-- This isn't as simple as using 'at' because the semantics aren't clear.
+--
+-- 1. Any index out of bounds leaves the object as-is
+-- 2. Setting works as you probably expect
+-- 3. Removal will __shift all later elements left__
 atNth :: Int -> AffineTraversal' Value (Maybe Value)
-atNth n = atraversal (nMatcher n) (nUpdater n)
+atNth n = atraversal (nMatcher n) $ nUpdater V.setAt n
 
+-- | A version of 'atNth' that treats setting as insertion
+--
+-- 1. Any index out of bounds leaves the object as-is
+-- 2. Removal works like 'atNth'
+-- 3. Setting (inserting) will __shift all later elements right__
 atNth' :: Int -> AffineTraversal' Value (Maybe Value)
-atNth' n = atraversal (nMatcher n) (nInserter n)
+atNth' n = atraversal (nMatcher n) $ nUpdater V.insertAt n
 
 nMatcher :: Int -> Value -> Either Value (Maybe Value)
 nMatcher n = \case
@@ -29,18 +50,19 @@ nMatcher n = \case
   Array vec -> Right $ vec V.!? n
   v -> Left v
 
-nUpdater :: Int -> Value -> Maybe Value -> Value
-nUpdater n nv = \case
-  Just x | Array vec <- nv -> Array $ V.setAt n x vec
-  v -> nInserter n nv v -- all other cases are the same
-
-nInserter :: Int -> Value -> Maybe Value -> Value
-nInserter n nv = \case
+nUpdater
+  :: (Int -> Value -> Vector Value -> Vector Value)
+  -- ^ How to update the vector, insertAt or setAt?
+  -> Int
+  -> Value
+  -> Maybe Value
+  -> Value
+nUpdater f n nv = \case
   Nothing -> case nv of
     Object km -> Object $ KeyMap.delete (Key.fromString $ show n) km
     Array vec -> Array $ V.deleteAt n vec
     v -> v
   Just x -> case nv of
     Object km -> Object $ KeyMap.insert (Key.fromString $ show n) x km
-    Array vec -> Array $ V.insertAt n x vec
+    Array vec -> Array $ f n x vec
     v -> v
