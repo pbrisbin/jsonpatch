ebafdf5cce4f4648a90f0938113a2544ada1abf0
patrick brisbin <pbrisbin@gmail.com>
2025-04-10 11:58:39 -0400

    chore: tighten up Apply formatting

--- src/Data/JSON/Patch/Prelude.hs
+++ src/Data/JSON/Patch/Prelude.hs
@@ -7,7 +7,7 @@ import Prelude as X
 
 import Control.Applicative as X (asum, (<|>))
 import Control.Exception as X (Exception (..))
-import Control.Monad as X (foldM, unless, void, when)
+import Control.Monad as X (foldM, guard, unless, void, when)
 import Data.Bifunctor as X (bimap, first, second)
 import Data.List.NonEmpty as X (NonEmpty, nonEmpty)
 import Data.Maybe as X (fromMaybe)
